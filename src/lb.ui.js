f253b99d2486b0d5ce73a04c68146e48b07f5fc9
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-04-19 12:15:05 +0200

    New module: lb.ui
    Added new module lb.ui, with unit test.
    
    Also, modified the order of targets in default build process, to avoid
    cleaning the output directory every time (now taking advantage of
    NaturalDocs caching) and generating the documentation first, which
    allows to review it while the javascript source files are checked by
    Jslint (which takes some time).

---
+++ src/lb.ui.js
@@ -0,0 +1,26 @@
+/*
+ * Namespace: lb.ui
+ * User Interface Modules for Legal Box Web Application
+ *
+ * Author:
+ * Eric Bréchemier <legalbox@eric.brechemier.name>
+ *
+ * Copyright:
+ * Legal Box (c) 2010, All Rights Reserved
+ *
+ * Version:
+ * 2010-04-19
+ */
+/*requires lb.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global window, lb */
+// preserve the module, if already loaded
+lb.ui = lb.ui || (function() {
+  // Builder of
+  // Closure for lb.ui namespace
+
+  // Note: no methods defined at this level currently
+
+  return { // public API
+  };
+}());
---
+++ test/test.lb.ui.js
@@ -0,0 +1,39 @@
+/*
+ * test.lb.ui.js - Unit Tests of lb.ui namespace
+ *
+ * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
+ * Copyright: Legal Box (c) 2010, All Rights Reserved
+ * Version:   2010-04-19
+ *
+ * Based on Test Runner from bezen.org JavaScript library
+ * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
+ */
+
+/*requires lb.ui.js */
+/*requires bezen.assert.js */
+/*requires bezen.object.js */
+/*requires bezen.testrunner.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global lb, bezen, window */
+window.test = (function() {
+  // Builder of
+  // Closure object for Test of lb.ui
+ 
+  // Define aliases
+  var assert = bezen.assert,
+      object = bezen.object,
+      testrunner = bezen.testrunner;
+
+  var testLbUi = function(){
+    
+    assert.isTrue( object.exists(window,'lb','ui'),     "lb.ui was not found");
+  };
+
+  var tests = {
+    testLbUi: testLbUi
+  };
+   
+  testrunner.define(tests, "lb.ui");
+  return tests;
+   
+}());
