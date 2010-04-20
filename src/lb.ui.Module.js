68c6a86f224e48a330b84a5211c8cc585626baf8
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-04-20 09:50:28 +0200

    New module: lb.ui.Module
    I added a module for User Interface Modules, with unit tests.

---
+++ src/lb.ui.Module.js
@@ -0,0 +1,118 @@
+/*
+ * Namespace: lb.ui.Module
+ * User Interface Modules of Legal Box Web Application
+ *
+ * Each User Interface Module corresponds to an independent unit of 
+ * functionality, which may be started and stopped.
+ * 
+ * For the purpose of this life cycle management, a new Module instance is 
+ * created for each User Interface Module creator registered on the application
+ * core Facade.
+ *
+ * Author:
+ * Eric Bréchemier <legalbox@eric.brechemier.name>
+ *
+ * Copyright:
+ * Legal Box (c) 2010, All Rights Reserved
+ *
+ * Version:
+ * 2010-04-19
+ */
+/*requires lb.ui.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global window, lb */
+// preserve the module, if already loaded
+lb.ui.Module = lb.ui.Module || function (name, creator, sandbox){
+  // Function: new Module(name,creator): Module
+  // Constructor of a new User Interface Module.
+  //
+  // Parameters:
+  //   name - string, the qualified module name, e.g. 'lb.ui.myModule'
+  //   creator - function, a creator function returning a UI module.
+  //             A new Sandbox instance will be provided as parameter.
+  //   sandbox - object, the sandbox instance for this UI module.
+  //
+  // Returns:
+  //   object, the new instance of lb.ui.Module
+
+  // string, the status of underlying module
+  var status;
+  
+  // object, the underlying module instance
+  var module;
+  
+  try {
+    module = creator(sandbox);
+    status = 'created';
+  } catch(e) {
+    sandbox.log('ERROR: Failed to create module "'+name+
+                '" using creator "'+creator+
+                '" with sandbox "'+sandbox+
+                '"; '+e+'.');
+    status = 'failed';
+  }
+
+  function getStatus(){
+    // Function: getStatus(): string
+    // Get the status of the underlying module
+    //
+    // Returns:
+    // - undefined initially
+    // - 'created' after a new instance gets created with provided creator
+    // - 'started' after the module gets started
+    // - 'stopped' after the module gets stopped
+    // - 'failed' after a failure in creator(), start(), stop() or notify()
+    
+    return status;
+  }
+
+  function start(){
+    // Function: start()
+    // Start the underlying module
+
+    try {
+      module.start();
+      status = 'started';
+    } catch(e){
+      sandbox.log('ERROR: Failed to start module "'+name+'"; '+e+'.');
+      status = 'failed';
+    }
+  }
+  
+  function stop(){
+    // Function: stop()
+    // Stop the underlying module
+
+    try {
+      module.stop();
+      status = 'stopped';
+    } catch(e){
+      sandbox.log('ERROR: Failed to stop module "'+name+'": '+e+'.');
+      status = 'failed';
+    }
+  }
+  
+  function notify(event){
+    // Function: notify(event)
+    // Notify an event to the underlying module
+    //
+    // Parameter:
+    //   event - object, the event subject of the notification
+
+    try {
+      module.notify(event);
+    } catch(e){
+      sandbox.log('ERROR: Failed to notify module "'+name+
+                  '" of event "'+event+
+                  '": '+e+'.');
+      status = 'failed';
+    }
+  }
+
+  // Public methods
+  this.getStatus = getStatus;
+  this.start = start;
+  this.stop = stop;
+  this.notify = notify;
+  return this;
+};
---
+++ test/test.lb.ui.Module.js
@@ -0,0 +1,192 @@
+/*
+ * test.lb.ui.Module.js - Unit Tests of User Interface Module
+ *
+ * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
+ * Copyright: Legal Box (c) 2010, All Rights Reserved
+ * Version:   2010-04-20
+ *
+ * Based on Test Runner from bezen.org JavaScript library
+ * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
+ */
+
+/*requires lb.ui.Module.js */
+/*requires bezen.assert.js */
+/*requires bezen.object.js */
+/*requires bezen.string.js */
+/*requires bezen.testrunner.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global lb, bezen, window */
+(function() {
+  // Builder of
+  // Closure object for Test of User Interface Module
+
+  // Define aliases
+  var assert = bezen.assert,
+      object = bezen.object,
+      string = bezen.string,
+      testrunner = bezen.testrunner;
+
+  function testNamespace(){
+
+    assert.isTrue( object.exists(window,'lb','ui','Module'),
+                                     "lb.ui.Module namespace was not found");
+  }
+
+  var startCounter = 0;
+  var stopCounter = 0;
+  var notifyEvents = [];
+
+  function createStubModule(){
+    // create a new stub module, for Unit Tests purpose,
+    // keeping track of calls to start() and stop(), as well as events provided
+    // to notify().
+
+    return {
+      start: function(){ startCounter++; },
+      stop: function(){ stopCounter++; },
+      notify: function(event){ notifyEvents.push(event); }
+    };
+  }
+
+  function creatorWhichFails(){
+    // a creator function that throws an exception
+
+    throw new Error('Test failure in creator');
+  }
+
+  function createFailingModule(){
+    // create a new stub module which fails in every method
+
+    return {
+      start: function(){ throw new Error('Test failure in start'); },
+      stop: function(){ throw new Error('Test failure in stop'); },
+      notify: function(event){ throw new Error('Test failure in notify'); }
+    };
+  }
+
+  var logMessages = [];
+  var stubSandbox = {
+    log: function(message){ logMessages.push(message); }
+  };
+
+  function testConstructor(){
+    // Unit tests for new lb.ui.Module()
+
+    var module = new lb.ui.Module('lb.ui.stub', createStubModule, stubSandbox);
+
+    logMessages = [];
+    module = new lb.ui.Module('lb.ui.fail', creatorWhichFails, stubSandbox);
+    assert.equals(logMessages.length, 1,     "one message expected in log");
+    assert.isTrue(string.startsWith(logMessages[0], 'ERROR: '),
+                                             "error message expected in log");
+  }
+
+  function testGetName(){
+    // Unit tests for lb.ui.Module#getName()
+
+    var name = 'lb.ui.stub';
+    var module = new lb.ui.Module(name, createStubModule);
+    assert.equals( module.getName(), name,
+                  "getName expected to return the name given in constructor");
+  }
+
+  function testStart(){
+    // Unit tests for lb.core.Module#start()
+
+    var module = new lb.ui.Module('lb.ui.stub', createStubModule);
+
+    startCounter = 0;
+    module.start();
+    assert.equals(startCounter, 1, "Underlying module expected to be started");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    logMessages = [];
+    module.start();
+    assert.equals(logMessages.length, 1,     "one message expected in log");
+    assert.isTrue(string.startsWith(logMessages[0], 'ERROR: '),
+                                             "error message expected in log");
+  }
+
+  function testStop(){
+    // Unit tests for lb.ui.Module#stop()
+
+    var module = new lb.ui.Module('lb.ui.stub', createStubModule);
+
+    module.start();
+
+    stopCounter = 0;
+    module.stop();
+    assert.equals(stopCounter, 1, "Underlying module expected to be stopped");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    logMessages = [];
+    module.start();
+    assert.equals(logMessages.length, 1,     "one message expected in log");
+    assert.isTrue(string.startsWith(logMessages[0], 'ERROR: '),
+                                             "error message expected in log");
+  }
+
+  function testNotify(){
+    // Unit tests for lb.ui.Module#notify()
+
+    var module = new lb.ui.Module('lb.ui.stub', createStubModule);
+    var event = {};
+
+    notifyEvents = [];
+    module.notify(event);
+    assert.arrayEquals(notifyEvents, [event],
+                        "Underlying module expected to be notified of event");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    logMessages = [];
+    module.notify(event);
+    assert.equals(logMessages.length, 1,     "one message expected in log");
+    assert.isTrue(string.startsWith(logMessages[0], 'ERROR: '),
+                                             "error message expected in log");
+  }
+
+  function testGetStatus(){
+    // Unit tests for lb.ui.Module#isStarted()
+
+    var module = new lb.ui.Module('lb.ui.module', createStubModule);
+    assert.equals( module.getStatus(), 'created', "wrong initial status");
+
+    module.start();
+    assert.equals( module.getStatus(), 'started',"wrong status after start()");
+
+    module.stop();
+    assert.equals( module.getStatus(), 'stopped', "wrong status after stop()");
+
+    module = new lb.ui.Module('lb.ui.fail', creatorWhichFails, stubSandbox);
+    assert.equals( module.getStatus(), 'failed',
+                                      "wrong status after failure in creator");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    module.start();
+    assert.equals( module.getStatus(), 'failed',
+                                      "wrong status after failure in start");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    module.stop();
+    assert.equals( module.getStatus(), 'failed',
+                                      "wrong status after failure in stop");
+
+    module = new lb.ui.Module('lb.ui.fail', createFailingModule, stubSandbox);
+    module.notify({});
+    assert.equals( module.getStatus(), 'failed',
+                                      "wrong status after failure in notify");
+  }
+
+  var tests = {
+    testNamespace: testNamespace,
+    testConstructor: testConstructor,
+    testStart: testStart,
+    testStop: testStop,
+    testNotify: testNotify,
+    testGetStatus: testGetStatus
+  };
+
+  testrunner.define(tests, "lb.ui.Module");
+  return tests;
+
+}());
