2c9753ecc18825b8aa3893ac5e02c8745f570250
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-04-29 10:27:09 +0200

    Module completed: lb.core.json
    I added unit tests and an implementation based on Google Closure
    Library.

--- src/lb.core.json.js
+++ src/lb.core.json.js
@@ -9,16 +9,53 @@
  * Legal Box (c) 2010, All Rights Reserved
  *
  * Version:
- * 2010-04-28
+ * 2010-04-29
  */
+/*requires closure/goog.json.js */
 /*requires lb.core.js */
 /*jslint nomen:false, white:false, onevar:false, plusplus:false */
-/*global lb */
+/*global lb, goog */
 // preserve the module, if already loaded
 lb.core.json = lb.core.json || (function() {
   // Builder of
   // Closure for lb.core.json module
 
+  // Declare alias
+  var json = goog.json;
+
+  function parse(string){
+    // Function: parse(string): object|array
+    // Parse a JSON string into corresponding object or array.
+    //
+    // Parameter:
+    //   string - string, a JSON string
+    //
+    // Returns:
+    //   object|array, the object or array resulting from parsing
+
+    //
+    return json.parse(string);
+  }
+
+  function serialize(object){
+    // Function: serialize(object): string
+    // Serialize a JSON object or array into a JSON string.
+    //
+    // Parameter:
+    //   object - object|array, a JavaScript object or array.
+    //            No function should be present in properties of a provided
+    //            object or any object within.
+    //
+    // Returns:
+    //   string, a string resulting from serialization of given object or array
+    // (end)
+
+    //
+    return json.serialize(object);
+  }
+
   return { // public API
+    parse: parse,
+    serialize: serialize
   };
 }());
--- test/test.lb.core.json.js
+++ test/test.lb.core.json.js
@@ -3,7 +3,7 @@
  *
  * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
  * Copyright: Legal Box (c) 2010, All Rights Reserved
- * Version:   2010-04-27
+ * Version:   2010-04-29
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -13,7 +13,7 @@
 /*requires bezen.assert.js */
 /*requires bezen.object.js */
 /*requires bezen.testrunner.js */
-/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false, evil:true */
 /*global lb, bezen, window */
 (function() {
   // Builder of
@@ -30,8 +30,84 @@
                                       "lb.core.json namespace was not found");
   }
 
+  function testSerialize(){
+    var ut = lb.core.json.serialize;
+
+    var object = {
+      string: 'one',
+      number: 2,
+      object: {
+        id: 3
+      },
+      array: [
+        'one',
+        2,
+        {id: 3},
+        true,
+        false,
+        null
+      ]
+    };
+    var jsonString = ut(object);
+    assert.isTrue( typeof jsonString === 'string',
+                        "serialization of JSON object must produce a string");
+
+    // Use unsafe parsing with eval to check result
+    assert.objectEquals( eval('('+jsonString+')'), object,
+ "object begot from unsafe parsing of JSON string must equal original object");
+
+    var array = [ {id:1}, {id:2}, {id:3} ];
+    assert.equals( ut(array), '[{"id":1},{"id":2},{"id":3}]',
+                                    "unexpected serialization of json array");
+  }
+
+  function testParse(){
+    var ut = lb.core.json.parse;
+
+    var string = '{'+
+      '"string": "one",'+
+      '"number": 2,'+
+      '"object": {'+
+        '"id": 3'+
+      '},'+
+      '"array": ['+
+        '"one",'+
+        '2,'+
+        '{"id": 3},'+
+        'true,'+
+        'false,'+
+        'null'+
+      ']'+
+    '}';
+
+    var object = {
+      string: 'one',
+      number: 2,
+      object: {
+        id: 3
+      },
+      array: [
+        'one',
+        2,
+        {id: 3},
+        true,
+        false,
+        null
+      ]
+    };
+
+    assert.objectEquals( ut(string), object,
+                    "parsed JSON string expected to equal reference object");
+
+    string = '[{"id":1},{"id":2},{"id":3}]';
+    assert.objectEquals( ut(string), [{id:1},{id:2},{id:3}],
+                     "parsed JSON string expected to equal reference array");
+  }
+
   var tests = {
-    testNamespace: testNamespace
+    testNamespace: testNamespace,
+    testSerialize: testSerialize,
+    testParse: testParse
   };
 
   testrunner.define(tests, "lb.core.json");
