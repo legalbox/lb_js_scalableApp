1a50394f72c2f52ad6ffcce0050e4a384246f315
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-05-03 10:14:30 +0200

    New module: lb.data
    This is a namespace for Data Model Modules of Legal Box Application.

---
+++ src/lb.data.js
@@ -0,0 +1,26 @@
+/*
+ * Namespace: lb.data
+ * Data Model Modules for Legal Box Web Application
+ *
+ * Author:
+ * Eric Bréchemier <legalbox@eric.brechemier.name>
+ *
+ * Copyright:
+ * Legal Box (c) 2010, All Rights Reserved
+ *
+ * Version:
+ * 2010-05-03
+ */
+/*requires lb.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global lb */
+// preserve the module, if already loaded
+lb.data = lb.data || (function() {
+  // Builder of
+  // Closure for lb.data namespace
+
+  // Note: no methods defined at this level currently
+
+  return { // public API
+  };
+}());
---
+++ test/test.lb.data.js
@@ -0,0 +1,40 @@
+/*
+ * test.lb.data.js - Unit Tests of lb.data namespace
+ *
+ * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
+ * Copyright: Legal Box (c) 2010, All Rights Reserved
+ * Version:   2010-05-03
+ *
+ * Based on Test Runner from bezen.org JavaScript library
+ * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
+ */
+
+/*requires lb.data.js */
+/*requires bezen.assert.js */
+/*requires bezen.object.js */
+/*requires bezen.testrunner.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global lb, bezen, window */
+(function() {
+  // Builder of
+  // Closure object for Test of lb.data
+ 
+  // Define aliases
+  var assert = bezen.assert,
+      object = bezen.object,
+      testrunner = bezen.testrunner;
+
+  function testNamespace(){
+
+    assert.isTrue( object.exists(window,'lb','data'),
+                                            "lb.data namespace was not found");
+  }
+
+  var tests = {
+    testNamespace: testNamespace
+  };
+
+  testrunner.define(tests, "lb.data");
+  return tests;
+
+}());
