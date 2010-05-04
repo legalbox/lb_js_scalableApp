c5f0b502a35555884ac6493d222c7b3ca210eebf
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-05-04 11:10:38 +0200

    Added methods to the Sandbox
    I added utility methods from the base library, and a method isInBox(),
    used internally as well, to check whether an element is part of the box.
    
    I also modified the implementation of getBox() (now caching the box
    element after first lookup) and getId() (converting local identifiers to
    full identifiers when an argument is provided)

--- src/lb.core.Sandbox.js
+++ src/lb.core.Sandbox.js
@@ -13,46 +13,126 @@
  * Legal Box (c) 2010, All Rights Reserved
  *
  * Version:
- * 2010-05-03
+ * 2010-05-04
  */
+/*requires lb.base.dom.js */
+/*requires lb.base.string.js */
+/*requires lb.base.log.js */
 /*requires lb.core.js */
+/*requires lb.core.events.publisher.js */
+/*requires lb.core.events.Subscriber.js */
 /*jslint nomen:false, white:false, onevar:false, plusplus:false */
 /*global lb */
 // preserve the module, if already loaded
-lb.core.Sandbox = lb.core.Sandbox || function (box,module,facade){
-  // Function: new Sandbox(box,module,facade): Sandbox
+lb.core.Sandbox = lb.core.Sandbox || function (id){
+  // Function: new Sandbox(id): Sandbox
   // Constructor of a new Sandbox.
   //
   // Parameters:
-  //   box - object, the root HTML element for this User Interface Module
-  //   module - object, the User Interface Module (instance of lb.ui.Module)
-  //   facade - object, the Application Core Facade to proxify
+  //   id - string, the identifier of the module, which is also the id of the 
+  //        root HTML element for this module. If the element does not exist in
+  //        the document, it will get created on the first call to getBox().
   //
   // Returns:
   //   object, the new instance of lb.core.Sandbox
 
+  // Define aliases
+  var dom = lb.base.dom,
+      trim = lb.base.string.trim,
+      log = lb.base.log.print,
+      publisher = lb.core.events.publisher,
+      Subscriber = lb.core.events.Subscriber,
+
+  // Private fields
+
+  // DOM element, the root of the box, carrying the module identifier.
+  // Used only withing getBox(), to avoid multiple lookups of the same element.
+  // Initialized on first call to getBox().
+      box;
+
+  function getId(localId){
+    // Function: getId(localId): string
+    // Get the identifier of the module, when optional parameter is omitted.
+    // With optional parameter, get the full identifier corresponding to the
+    // given local identifier.
+    //
+    // Parameter:
+    //  localId - string, optional local identifier
+    //
+    // Returns:
+    //   string, the identifier of the module, as provided in constructor,
+    //   or the full identifier corresponding to given local identifier.
+    //
+    // Note:
+    //   The full identifier is made of the module identifier, followed by the
+    //   separator '.', followed by the local identifier.
+
+    if (localId) {
+      return id+'.'+localId;
+    } else {
+      return id;
+    }
+  }
+
   function getBox(){
-    // Function: getBox(): object
+    // Function: getBox(): DOM element
     // Get the root HTML element for this module
     //
     // Returns:
-    //   object, the box element provided in constructor
+    //   DOM element, the HTML element corresponding to the module id
+    //
+    // Note:
+    //   In case no HTML element is found in the document with the module id,
+    //   a new div element is created with this id and inserted last in the
+    //   document body, and this new element is returned.
 
+    if (box) {
+      return box;
+    }
+    box = dom.$(id);
+    if (!box){
+      log('Warning: no element "'+id+
+          '" found in box. Will be created at end of body.');
+      box = dom.element('div',{'id': id});
+      document.body.appendChild(box);
+    }
     return box;
   }
 
-  function subscribe(event,callback){
-    // Function: subscribe(event,callback)
-    // Attach a callback for events of given type.
+  function isInBox(element){
+    // Function: isInBox(element): boolean
+    // Check whether the given element is in the box.
     //
-    // The User Interface Module will get subscribed to all events on the
-    // Application Core Facade. Each event will get notified to the Module
-    // by the Facade. The Module will then filter events and trigger the
-    // provided callback in case the event has the same properties as the
-    // event.
+    // Parameter:
+    //   element - DOM element, an element
+    //
+    // Returns:
+    //   true if the element is a descendant of or the root of the box itself
+    //   false otherwise
+
+    // Warning: element attribute hides element() function.
+
+    var ancestor = element;
+    while (ancestor) {
+      // box must be found in ancestors or self
+      if ( ancestor === getBox() ) {
+        return true;
+      }
+      ancestor = ancestor.parentNode;
+    }
+    return false;
+  }
+
+  function subscribe(filter,callback){
+    // Function: subscribe(filter,callback)
+    // Create a new event subscription, triggering the callback only for events
+    // matching the provided filter.
+    //
+    // A new instance of Event Subscriber (lb.core.events.Subscriber) is
+    // created and added to the Event publisher (lb.core.events.publisher).
     //
     // Parameters:
-    //   event - object, the type of event.
+    //   filter - object, the event filter.
     //           This object is similar to event objects. Any included property
     //           will be used as a filter to restrict events part of the 
     //           subscription. For example:
@@ -61,35 +141,197 @@ lb.core.Sandbox = lb.core.Sandbox || function (box,module,facade){
     //           * {name: 'foo', id:42} filters on name==='foo' and id===42
     //   callback - function, the associated callback function
 
-    // subscribe the associated User Interface Module to this event
-    // 1. add a filter for this type of event, associated with given callback
-    module.subscribe(event,callback);
-    // 2. subscribe the module to all events
-    facade.subscribe(module);
+    publisher.addSubscriber( new Subscriber(filter,callback) );
   }
 
-  function proxify(api){
-    // (private) proxify(api)
-    // Copy api methods to this object
+  function trim(string){
+    // Function: trim(string): string
+    // Remove leading and trailing whitespace from a string.
+    //
+    // Parameter:
+    //   string - string, the string to trim
+    //
+    // Returns:
+    //   string, a copy of the string with no whitespace at start and end
+
+    return trim(string);
+  }
+
+  function $(localId){
+    // Function: $(localId): DOM element
+    // Get the element of the box with given local identifier
+    //
+    // Parameter:
+    //   localId - string, the local identifier of the element, without prefix.
+    //             See getId() for details.
+    //
+    // Returns:
+    //   * DOM element, the element from the box with corresponding localId
+    //   * null if no element is found in the box with the localId
+    //
+    // Notes:
+    //   This method calls getBox() internally to check that a found element
+    //   is a descendant of the box element. This may create the box element
+    //   if not already present in the document.
+    //
+    //   Since the provided localId is converted to a full identifier using
+    //   getId(), a call to $() without argument will return the root element
+    //   of the box, in the same way as getBox().
+
+    var element = dom.$( getId(localId) );
+    if ( isInBox(element) ){
+      return element;
+    }
+    log('Warning: element "'+getId(localId)+'" not part of box "'+id+'"');
+    return null;
+  }
+
+  function element(name,attributes){
+    // Function: element(name,attributes,childNodes): DOM element
+    // Create a new DOM element
+    // e.g.
+    // element('a',{href:'#here',title:'Here'},'Click here')
+    // will create a new DOM element
+    // <a href='#here' title='Here'>Click here</a>
     //
     // Parameters:
-    //   api - object, the application programming interface object.
-    //         Each field of this object is expected to be a function
-    //         to be copied to the sandbox. No method of this object should
-    //         be named like native methods of the sandbox (getBox, subscribe);
-    //         otherwise, they will be hidden by the sandbox methods.
+    //   name - string, the name of the element
+    //   attributes - object, optional arguments as a set of named properties
+    //   childNodes - array or list of arguments, the optional child nodes.
+    //                Text nodes may be represented simply as strings.
+    //
+    // Returns:
+    //   DOM element, the newly created DOM element
 
-    for (var name in api){
-      if ( api.hasOwnProperty(name) ) {
-        this[name] = api[name];
-      }
-    }
+    return dom.element.apply(this,arguments);
   }
 
-  // Public methods
-  proxify.call(this, facade.getApi() );
-  this.getBox = getBox;
-  this.subscribe = subscribe;
+  function getClasses(element){
+    // Function: getClasses(element): object
+    // Get the CSS classes of given DOM element
+    //
+    // Parameter:
+    //   element - DOM element, an element of the box
+    //
+    // Returns:
+    //   object, a hash of CSS classes, with a boolean property set to true
+    //   for each of the CSS class names found on element.
+    //
+    // Note:
+    //   an empty object is returned when the element is out of the box.
 
-  return this;
+    // Warning: element parameter hides element() function
+    if ( !isInBox(element) ){
+      log('Warning: cannot get CSS classes of element "'+element+
+          '" outside of box "'+id+'"');
+      return {};
+    }
+
+    return dom.getClasses(element);
+  }
+
+  function addClass(element,name){
+    // Function: addClass(element,name)
+    // Append a CSS class to a DOM element part of the box
+    //
+    // Parameters:
+    //   element - DOM element, an element of the box
+    //   name - string, a CSS class name
+    //
+    // Note:
+    //   Nothing happens if element is out of the box.
+
+    // Warning: element parameter hides element() function
+    if ( !isInBox(element) ){
+      log('Warning: cannot add CSS class to element "'+element+
+          '" outside of box "'+id+'"');
+      return;
+    }
+
+    dom.addClass(element,name);
+  }
+
+  function removeClass(element,name){
+    // Function: removeClass(element,name)
+    // Remove a CSS class from a DOM element part of the box
+    //
+    // Parameters:
+    //   element - DOM element, an element of the box
+    //   name - string, a CSS class name
+    //
+    // Note:
+    //   Nothing happens if element is out of the box.
+
+    // Warning: element parameter hides element() function
+    if ( !isInBox(element) ){
+      log('Warning: cannot remove CSS class from element "'+element+
+          '" outside of box "'+id+'"');
+      return;
+    }
+
+    dom.removeClass(element,name);
+  }
+
+  function addListener(element,type,listener,useCapture){
+    // Function: addListener(element, type, listener, useCapture)
+    // Register a new listener for a type of event on a DOM element of the box
+    //
+    // Parameters:
+    //   element - Element, a DOM element
+    //   type - string, the name of an event (without 'on') e.g. 'click'
+    //   listener - function, a function to call when the event is dispatched.
+    //   useCapture - boolean, whether the callback is set for capture phase.
+    //                Optional: defaults to false.
+    //
+    // Note:
+    //   Nothing happens when element is out of the box.
+
+    // Warning: element parameter hides element() function
+    if ( !isInBox(element) ){
+      log('Warning: cannot add listener to element "'+element+
+          '" outside of box "'+id+'"');
+      return;
+    }
+
+    dom.addListener(element,type,listener,useCapture);
+  }
+
+  function removeListener(element,type,listener,useCapture){
+    // Function: removeListener(element, type, listener, useCapture)
+    // Unregister a listener for a type of event on a DOM element of the box
+    //
+    // Parameters:
+    //   element - Element, a DOM element
+    //   type - string, the name of an event (without 'on') e.g. 'click'
+    //   listener - function, a function to call when the event is dispatched.
+    //   useCapture - boolean, whether the callback is set for capture phase.
+    //                Optional: defaults to false.
+    //
+    // Note:
+    //   Nothing happens when element is out of the box.
+
+    // Warning: element parameter hides element() function
+    if ( !isInBox(element) ){
+      log('Warning: cannot remove listener from element "'+element+
+          '" outside of box "'+id+'"');
+      return;
+    }
+
+    dom.removeListener(element,type,listener,useCapture);
+  }
+
+  return { // Public methods
+    getId: getId,
+    getBox: getBox,
+    isInBox: isInBox,
+    subscribe: subscribe,
+    trim: trim,
+    $: $,
+    element: element,
+    getClasses: getClasses,
+    addClass: addClass,
+    removeClass: removeClass,
+    addListener: addListener,
+    removeListener: removeListener
+  };
 };
--- test/test.lb.core.Sandbox.js
+++ test/test.lb.core.Sandbox.js
@@ -3,18 +3,23 @@
  *
  * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
  * Copyright: Legal Box (c) 2010, All Rights Reserved
- * Version:   2010-05-03
+ * Version:   2010-05-04
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
  */
 
 /*requires lb.core.Sandbox.js */
+/*requires lb.core.events.publisher.js */
+/*requires bezen.js */
+/*requires bezen.string.js */
+/*requires bezen.dom.js */
 /*requires bezen.assert.js */
 /*requires bezen.object.js */
 /*requires bezen.testrunner.js */
+/*requires goog.events.js */
 /*jslint nomen:false, white:false, onevar:false, plusplus:false */
-/*global lb, bezen, window */
+/*global lb, bezen, window, document, goog */
 (function() {
   // Builder of
   // Closure object for Test of User Interface Module
@@ -22,7 +27,13 @@
   // Define aliases
   var assert = bezen.assert,
       object = bezen.object,
-      testrunner = bezen.testrunner;
+      testrunner = bezen.testrunner,
+      $ = bezen.$,
+      endsWith = bezen.string.endsWith,
+      ELEMENT_NODE = bezen.dom.ELEMENT_NODE,
+      TEXT_NODE = bezen.dom.TEXT_NODE,
+      element = bezen.dom.element,
+      events = goog.events;
 
   function testNamespace(){
 
@@ -30,91 +41,218 @@
                                    "lb.core.Sandbox namespace was not found");
   }
 
-  var subscribedModules = [];
-
-  function stubFunc1(){ // stub function #1
-  }
-  function stubFunc2(){ // stub function #2
-  }
-  function stubFunc3(){ // stub function #3
-  }
-
-  // stub Facade object, for unit tests
-  var stubFacade = {
-    subscribe: function(module){
-      subscribedModules.push(module);
-    },
-    getApi: function(){
-      return {
-        func1: stubFunc1,
-        func2: stubFunc2,
-        func3: stubFunc3
-      };
-    }
-  };
-
-  var eventFilters = [];
-  var eventCallbacks = [];
-
-  // stub Module object, for unit tests
-  var stubModule = {
-    subscribe: function(event,callback){
-      eventFilters.push(event);
-      eventCallbacks.push(callback);
-    }
-  };
-
   function testConstructor(){
     var Ut = lb.core.Sandbox;
 
-    var sandbox = new Ut('testId', stubModule, stubFacade);
+    var sandbox = new Ut('myId');
+  }
 
-    // Note:
-    // in current implementation, it is expected that the proxified functions
-    // are simply copied to the sandbox object. An more complex intermediation
-    // may be put in place in the future, e.g. to have this reference the
-    // facade during the call.
-    assert.equals( sandbox.func1, stubFunc1,
-                                         "function #1 expected to be copied");
-    assert.equals( sandbox.func2, stubFunc2,
-                                         "function #2 expected to be copied");
-    assert.equals( sandbox.func3, stubFunc3,
-                                         "function #3 expected to be copied");
+  function testGetId(){
+    // Unit tests for lb.core.Sandbox#getId()
+
+    var testId = 'lb.ui.myModule';
+    var sandbox = new lb.core.Sandbox(testId);
+    assert.equals( sandbox.getId(), testId,
+                                  "id must match value given in constructor");
+    assert.isFalse( object.exists( $('lb.ui.myModule') ),
+                    "box element must not be created until getBox is called");
   }
 
   function testGetBox(){
     // Unit tests for lb.core.Sandbox#getBox()
 
-    var box = {};
-    var sandbox = new lb.core.Sandbox(box, stubModule, stubFacade);
-    assert.equals( sandbox.getBox(), box,
-                                      "box provided in constructor expected");
+    var sandbox = new lb.core.Sandbox('testGetBox');
+    assert.equals( sandbox.getBox(), $('testGetBox'),
+                               "box element 'testGetBox' should be returned");
+
+    sandbox = new lb.core.Sandbox('missing');
+    var box = sandbox.getBox();
+    assert.isTrue( object.exists(box),         "missing box must be created");
+    assert.equals(box, $('missing'),   "new element must be created with id");
+    assert.equals(box.nodeType,ELEMENT_NODE,
+                                       "new element must be an element node");
+    assert.equals(box.nodeName, 'DIV',           "new element must be a DIV");
+    assert.equals(box.parentNode, document.body,
+                              "new element must be created in document.body");
+    assert.isFalse( object.exists(box.nextSibling),
+                              "new element must be last in document.body");
+  }
+
+  function testIsInBox(){
+    var ut = new lb.core.Sandbox('testIsInBox').isInBox;
+
+    assert.isFalse( ut(null),                      "false expected for null");
+    assert.isFalse( ut(undefined),            "false expected for undefined");
+    assert.isTrue( ut( $('testIsInBox.inBox') ),
+                                           "true expected for element in box");
+    assert.isFalse( ut( $('testIsInBox.outsideBox') ),
+                                     "false expected for element outside box");
+    assert.isFalse( ut(document.body),  "false expected for ancestor element");
+    assert.isFalse( ut( element('div') ),
+                                     "false expected for element outside DOM");
   }
 
   function testSubscribe(){
-    // Unit tests for lb.core.Sandbox#notify()
+    var ut = new lb.core.Sandbox('testSubscribe').subscribe;
 
-    var sandbox = new lb.core.Sandbox('testId', stubModule, stubFacade);
+    lb.core.events.publisher.getSubscribers().length = 0;
 
-    eventFilters = [];
-    eventCallbacks = [];
-    subscribedModules = [];
-    var event = {};
-    var callback = function(){};
-    sandbox.subscribe(event,callback);
-    assert.arrayEquals(eventFilters, [event],
-                                "event filter expected on module");
-    assert.arrayEquals(eventCallbacks, [callback],
-                                "event callback expected on module");
-    assert.arrayEquals(subscribedModules,[stubModule],
-                                "module expected to be subscribed on facade");
+    var notifiedEvents = [];
+    var callback = function(event){
+      notifiedEvents.push(event);
+    };
+    var filter = {};
+    ut(filter,callback);
+
+    assert.equals(lb.core.events.publisher.getSubscribers().length, 1,
+                                          "one new event Subscriber expected");
+    var event1 = {};
+    lb.core.events.publisher.publish(event1);
+    assert.arrayEquals(notifiedEvents, [event1],
+                                "callback expected to be notified of event 1");
+  }
+
+  function testTrim(){
+    var ut = new lb.core.Sandbox('testTrim').trim;
+
+    assert.equals( ut('abcd'), 'abcd',
+                          "no change expected when no whitespace is present");
+    assert.equals( ut('a\nb c\td'), 'a\nb c\td',
+                                     "internal whitespace must be preserved");
+    assert.equals( ut('  \n\t  abcd  \n\t  '), 'abcd',
+                                  "whitespace must be removed on both sides");
+  }
+
+  function test$(){
+    var ut = new lb.core.Sandbox('test$').$;
+
+    assert.equals( ut('testId'), document.getElementById('test$.testId'),
+      "$ must return same node as document.getElementById, once prefix added");
+
+    assert.equals( ut('outsideBox'), null,
+                                      "$ must not return element outside box");
+  }
+
+  function testElement(){
+    var ut = new lb.core.Sandbox('testElement').element;
+
+    var a1 = ut('a',{href:"#first"},"first link");
+    assert.equals( a1.nodeType, ELEMENT_NODE,
+                                                "a1 must have type ELEMENT");
+    assert.equals( a1.nodeName, "A",            "a1 must have name A");
+    assert.isTrue( endsWith(a1.href,'#first'),  "a1 must have href set");
+    assert.equals( a1.childNodes.length, 1,     "a1 must have 1 child");
+    var t1 = a1.firstChild;
+    assert.equals( t1.nodeType, TEXT_NODE, "t1 must have type TEXT");
+    assert.equals( t1.data, "first link",       "t1 data must be set");
+  }
+
+  function testGetClasses(){
+    var ut = new lb.core.Sandbox('testGetClasses').getClasses;
+
+    assert.objectEquals( ut( $('testGetClasses.threeClasses') ),
+                         {'one':true, 'two':true, 'three':true},
+                                            "three classes expected in hash");
+
+    assert.objectEquals( ut( $('testGetClasses.outsideBox') ), {},
+                            "empty hash expected when element is out of box");
+  }
+
+  function testAddClass(){
+    var ut = new lb.core.Sandbox('testAddClass').addClass;
+
+    var div = $('testAddClass.noClass');
+    ut(div, 'one');
+    ut(div, 'two');
+    ut(div, 'three');
+    assert.equals(div.className, 'one two three',
+                         "'one two three' expected in class after third add");
+
+    div = $('testAddClass.outsideBox');
+    ut(div, 'three');
+    assert.equals(div.className, 'one two',
+                             "no change expected when element is out of box");
+  }
+
+  function testRemoveClass(){
+    var ut = new lb.core.Sandbox('testRemoveClass').removeClass;
+
+    var div = $('testRemoveClass.threeClasses');
+    ut(div, 'two');
+    assert.equals(div.className, 'one three',
+                                        "class 'two' expected to be removed");
+
+    div = $('testRemoveClass.outsideBox');
+    ut(div, 'two');
+    assert.equals(div.className, 'one two three',
+                             "no change expected when element is out of box");
+  }
+
+  function testAddListener(){
+    var ut = new lb.core.Sandbox('testAddListener').addListener;
+
+    var div1 = $('testAddListener.click');
+    var events1 = [];
+    var callback1 = function(event){
+      events1.push(event);
+    };
+    ut(div1, 'click', callback1, true);
+    var listeners1 = events.getListeners(div1, 'click', true);
+    assert.equals(listeners1.length, 1,
+                                   "one capturing listener expected on div1");
+    var event1 = {};
+    listeners1[0].handleEvent(event1);
+    assert.arrayEquals(events1,[event1],
+                            "callback expected to be triggered with event1");
+
+    var div2 = $('testAddListener.outsideBox');
+    var events2 = [];
+    var callback2 = function(event){
+      events2.push(event);
+    };
+    ut(div2, 'click', callback2, true);
+    var listeners2 = events.getListeners(div2, 'click', true);
+    assert.equals(listeners2.length, 0,
+                           "no listener expected on div2 outside of the box");
+  }
+
+  function testRemoveListener(){
+    var ut = new lb.core.Sandbox('testRemoveListener').removeListener;
+
+    var div1 = $('testRemoveListener.click');
+    var callback1 = function(){};
+    events.listen(div1, 'click', callback1);
+    assert.equals( events.getListeners(div1,'click',false).length, 1,
+                            "assert: non-capturing listener must be set");
+    ut(div1, 'click', callback1);
+    assert.arrayEquals( events.getListeners(div1,'click',false), [],
+                            "non-capturing listener expected to be removed.");
+
+    var div2 = $('testRemoveListener.outsideBox');
+    var callback2 = function(){};
+    events.listen(div2, 'click', callback2);
+    assert.equals( events.getListeners(div2,'click',false).length, 1,
+                            "assert: non-capturing listener must be set");
+    ut(div2, 'click', callback2);
+    assert.arrayEquals( events.getListeners(div2,'click',false).length, 1,
+                            "no change expected when element is out of box.");
   }
 
   var tests = {
     testNamespace: testNamespace,
     testConstructor: testConstructor,
+    testGetId: testGetId,
     testGetBox: testGetBox,
-    testSubscribe: testSubscribe
+    testIsInBox: testIsInBox,
+    testSubscribe: testSubscribe,
+    testTrim: testTrim,
+    test$: test$,
+    testElement: testElement,
+    testGetClasses: testGetClasses,
+    testAddClass: testAddClass,
+    testRemoveClass: testRemoveClass,
+    testaddListener: testAddListener,
+    testRemoveListener: testRemoveListener
   };
 
   testrunner.define(tests, "lb.core.Sandbox");
