7e0d2b573a3c6a324c4d967285dd27a7255b8ecb
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-05-31 12:17:03 +0200

    New module: lb.base.config, for configuration properties
    This module is intended for the configuration of specific properties
    expected by modules for the framework core and other modules, such as
    Rich Internet Application widgets. Some properties depend on the
    cross-browser library chosen for the base layer, currently Google
    Closure. Setting generic properties in an independent cache avoids:
    - the need for specific methods in the API for the need of a specific
    implementation
    - a dependency from base modules, the Sandbox and RIA widgets to the
    application core (where properties will be set by user code)

---
+++ test/test.lb.base.config.js
@@ -0,0 +1,114 @@
+/*
+ * test.lb.base.config.js - Unit Tests of lb.base.config module
+ *
+ * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
+ * Copyright: Legal Box (c) 2010, All Rights Reserved
+ * Version:   2010-05-31
+ *
+ * Based on Test Runner from bezen.org JavaScript library
+ * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
+ */
+
+/*requires lb.base.config.js */
+/*requires bezen.assert.js */
+/*requires bezen.object.js */
+/*requires bezen.testrunner.js */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false, evil:true */
+/*global lb, bezen, window */
+(function() {
+  // Builder of
+  // Closure object for Test of lb.base.config
+
+  // Define aliases
+  var assert = bezen.assert,
+      object = bezen.object,
+      testrunner = bezen.testrunner;
+
+  function testNamespace(){
+
+    assert.isTrue( object.exists(window,'lb','base','config'),
+                                    "lb.base.config namespace was not found");
+  }
+
+  function testReset(){
+    var ut = lb.base.config.reset;
+
+    lb.base.config.setOptions({a:1, b:2, c:'three'});
+    ut();
+    assert.equals( lb.base.config.getOption('a'), null,
+                                                    "a expected to be reset");
+    assert.equals( lb.base.config.getOption('b'), null,
+                                                    "b expected to be reset");
+    assert.equals( lb.base.config.getOption('c'), null,
+                                                    "c expected to be reset");
+  }
+
+  function testSetOptions(){
+    var ut = lb.base.config.setOptions;
+
+    lb.base.config.reset();
+    var three = {id:33};
+    ut({a:1, b:'two', c:three, d: true});
+    assert.equals( lb.base.config.getOption('a'), 1,   "a expected to be set");
+    assert.equals( lb.base.config.getOption('b'), 'two',
+                                                       "b expected to be set");
+    assert.equals( lb.base.config.getOption('c'), three,
+                                                       "c expected to be set");
+    assert.equals( lb.base.config.getOption('d'), true,
+                                                       "d expected to be set");
+
+    ut({b:'B', c:undefined, d:false});
+    assert.equals( lb.base.config.getOption('a'), 1,
+                                                 "a expected to be preserved");
+    assert.equals( lb.base.config.getOption('b'), 'B',
+                                                  "b expected to be replaced");
+    assert.equals( lb.base.config.getOption('c'), null,
+                                                  "c expected to be removed");
+    assert.equals( lb.base.config.getOption('d'), false,
+                                             "d expected to be set to false");
+  }
+
+  function testGetOption(){
+    var ut = lb.base.config.getOption;
+
+    lb.base.config.reset();
+    var value = {id:42};
+    lb.base.config.setOptions({
+      a: value,
+      b:false,
+      c:0,
+      d:'',
+      e:null,
+      f:undefined
+    });
+
+    assert.equals( ut('a'), value,   "defined value expected to be returned");
+    assert.equals( ut('missing'), null,
+                           "null expected for missing value without default");
+    var defaultValue = {id:777};
+    assert.equals( ut('missing',defaultValue), defaultValue,
+                         "provided default value expected for missing value");
+
+    assert.equals( ut('b'), false,                    "false value expected");
+    assert.equals( ut('c'), 0,                        "zero expected");
+    assert.equals( ut('d'), '',                       "empty string expected");
+    assert.equals( ut('e'), null,                     "null value expected");
+    assert.equals( ut('f'), null,                     "null value expected");
+
+    assert.equals( ut('e',defaultValue), defaultValue,
+                                   "default value expected for null value");
+    assert.equals( ut('f',defaultValue), defaultValue,
+                              "default value expected for undefined value");
+  }
+
+  var tests = {
+    testNamespace: testNamespace,
+    testReset: testReset,
+    testSetOptions: testSetOptions,
+    testGetOption: testGetOption
+  };
+
+  testrunner.define(tests, "lb.base.config");
+  return tests;
+
+}());
