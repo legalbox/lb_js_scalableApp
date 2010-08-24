474fc11c172aecef7bb47133c000832d78ee7b76
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-08-24 15:19:14 +0200

    Added base module for templates

---
+++ src/lb.base.template.js
@@ -0,0 +1,132 @@
+/*
+ * Namespace: lb.base.template
+ * Base Template Module
+ *
+ * This module provides the basis for HTML templates using a set of functions
+ * as filters to modify the input. See applyFilters() for details.
+ *
+ * Author:
+ * Eric Bréchemier <legalbox@eric.brechemier.name>
+ *
+ * Copyright:
+ * Legal Box SAS (c) 2010, All Rights Reserved
+ *
+ * License:
+ * BSD License
+ * http://creativecommons.org/licenses/BSD/
+ *
+ * Version:
+ * 2010-08-24
+ */
+/*requires lb.base.js */
+/*jslint white:false, plusplus:false */
+/*global lb, goog */
+// preserve the module, if already loaded
+lb.base.template = lb.base.template || (function() {
+  // Builder of
+  // Closure for lb.base.template module
+
+  function applyFilters(filters,input){
+    // Function: applyFilters(filters,input...): any
+    // Apply filters successively to input made of following arguments.
+    //
+    // This method may be applied to several types of input, e.g. strings or
+    // DOM nodes, using different sets of filters according to expected types.
+    //
+    // Design of HTML Templates:
+    // Here is a proposed solution for HTML Templates using this method.
+    // The input would be a DOM node and an optional context object.
+    // | var node = element('span',{},'Welcome #name#');
+    // | applyFilters(
+    // |   filters,
+    // |   node,
+    // |   {
+    // |     language:'en-US',
+    // |     data:
+    // |     {
+    // |       name:'John Doe'
+    // |     }
+    // |   }
+    // | );
+    // The first filter may implement top-down parsing in the following way:
+    // | var ELEMENT_NODE = 1;
+    // | function topDownParsing(filters,node,context){
+    // |   if (!node || node.nodeType!==ELEMENT_NODE){
+    // |     return;
+    // |   }
+    // |   var i, length, attribute, child;
+    // |   for (i=0, length=node.attributes.length; i<length; i++){
+    // |     attribute = node.attributes[i];
+    // |     applyFilters(filters,attribute,context);
+    // |   }
+    // |   for (i=0, length=node.childNodes.length; i<length; i++){
+    // |     child = node.childNodes[i];
+    // |     applyFilters(filters,child,context);
+    // |   }
+    // | }
+    // A more specific filter may operate the replacement of parameter values:
+    // | var PARAM_REGEXP = /#([a-zA-Z0-9\-]+)#/g;
+    // | function replaceParams(filters,node,context){
+    // |   if ( !node || !node.nodeValue || !node.nodeValue.replace ||
+    // |        !context || !context.data){
+    // |     return;
+    // |   }
+    // |   node.nodeValue = node.nodeValue.replace(
+    // |     PARAM_REGEXP,
+    // |     function(match,param){
+    // |       return context.data[param];
+    // |     }
+    // |   );
+    // | }
+    //
+    // Design of String Templates:
+    // This is an alternate template system, using as input a string and an
+    // optional object for values of parameters to replace in the string.
+    // | var greeting = applyFilters(
+    // |                  filters,
+    // |                  'Welcome #name#',
+    // |                  {name: 'John Doe'}
+    // |                );
+    //
+    // A single filter may be provided here to operate the replacement,
+    // rewriting replaceParams from the previous example to adapt it to the new
+    // input types:
+    // | function replaceParamsInString(filters, string, data){
+    // |   return string.replace(PARAM_REGEXP, function(match,param){
+    // |     return data[param];
+    // |   });
+    // | }
+    //
+    // Parameters:
+    //   filters - array, list of function filters, ordered from less specific
+    //             to most specific. Each filter will be provided the same
+    //             arguments present in the call to applyFilters(). Its return
+    //             value is interpreted in the following way:
+    //             o true or any truthy value to stop the processing
+    //             o undefined or any falsy value to continue with next filter
+    //   input... - variable number of arguments for input or context
+    //
+    // Returns:
+    //   undefined or any value returned by the last filter run.
+    //
+    // Note:
+    // Filters are applied from last (most specific) to first (less specific).
+    // Unless processing is interrupted by a filter returning a truthy value,
+    // all filters will be applied in turn, in this order.
+
+    if (!filters){
+      return;
+    }
+    var i, result;
+    for (i=filters.length-1; i>=0; i--){
+      result = filters[i].apply(this,arguments);
+      if (result){
+        return result;
+      }
+    }
+  }
+
+  return { // public API
+    applyFilters: applyFilters
+  };
+}());
---
+++ test/test.lb.base.template.js
@@ -0,0 +1,198 @@
+/*
+ * test.lb.base.template.js - Unit Tests of lb.base.template module
+ *
+ * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
+ * Copyright: Legal Box (c) 2010, All Rights Reserved
+ * License:   BSD License - http://creativecommons.org/licenses/BSD/
+ * Version:   2010-08-24
+ *
+ * Based on Test Runner from bezen.org JavaScript library
+ * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
+ */
+
+/*requires lb.base.template.js */
+/*jslint white:false, onevar:false, plusplus:false */
+/*global lb, bezen, window */
+(function() {
+  // Builder of
+  // Closure object for Test of lb.base.template
+
+  // Define aliases
+      /*requires bezen.assert.js */
+  var assert = bezen.assert,
+      /*requires bezen.object.js */
+      object = bezen.object,
+      /*requires bezen.testrunner.js */
+      testrunner = bezen.testrunner,
+      /*requires bezen.dom.js*/
+      element = bezen.dom.element;
+
+  function testNamespace(){
+
+    assert.isTrue( object.exists(window,'lb','base','template'),
+                                  "lb.base.template namespace was not found");
+  }
+
+  function testApplyFilters(){
+    var ut = lb.base.template.applyFilters;
+
+    try {
+      ut();
+    }catch(noFilters){
+      assert.fail("No error expected without filter argument: "+noFilters);
+    }
+
+    try {
+      ut([]);
+    }catch(emptyFilters){
+      assert.fail("No error expected with empty filters array: "+emptyFilters);
+    }
+
+    var captured = [];
+    function catchFilter(){
+      captured.push.apply(captured,arguments);
+    }
+
+    var arg1 = {}, arg2 = 'XYZ', arg3 = /abc/g;
+    var filters = [catchFilter];
+    ut(filters,arg1,arg2,arg3);
+    assert.arrayEquals(captured, [filters,arg1,arg2,arg3],
+                                     "3 arguments expected to be captured");
+
+    captured = [];
+    filters = [catchFilter];
+    ut(filters, arg1,arg2,arg3, arg1,arg2,arg3, arg1,arg2,arg3);
+    assert.arrayEquals(captured, [filters,
+                                  arg1,arg2,arg3,
+                                  arg1,arg2,arg3,
+                                  arg1,arg2,arg3],
+                                     "9 arguments expected to be captured");
+
+    function returnFalse(){
+      return false;
+    }
+    function returnTrue(){
+      return true;
+    }
+    function returnUndefined(){
+      return;
+    }
+
+    captured = [];
+    filters = [catchFilter, returnTrue];
+    ut(filters);
+    assert.arrayEquals(captured,[],
+            "processing expected to stop when previous filter returns true");
+
+    captured = [];
+    filters = [catchFilter, returnFalse];
+    ut(filters);
+    assert.equals(captured.length, 1,
+         "processing expected to continue when previous filter returns false");
+
+    captured = [];
+    filters = [catchFilter, returnUndefined];
+    ut(filters);
+    assert.equals(captured.length, 1,
+     "processing expected to continue when previous filter returns undefined");
+
+    captured = [];
+    filters = [
+                 catchFilter,
+                 returnTrue,
+                 catchFilter,
+                 returnFalse,
+                 catchFilter,
+                 returnUndefined,
+                 catchFilter
+              ];
+    ut(filters);
+    assert.equals(captured.length, 3,
+                   "filters expected to be triggered until one returns true");
+
+    // Test functions defined in documentation of the method
+    var applyFilters = ut;
+    var ELEMENT_NODE = 1;
+    function topDownParsing(filters,node,context){
+      if (!node || node.nodeType!==ELEMENT_NODE){
+        return;
+      }
+      var i, length, attribute, child;
+      for (i=0, length=node.attributes.length; i<length; i++){
+        attribute = node.attributes[i];
+        applyFilters(filters,attribute,context);
+      }
+      for (i=0, length=node.childNodes.length; i<length; i++){
+        child = node.childNodes[i];
+        applyFilters(filters,child,context);
+      }
+    }
+
+    var PARAM_REGEXP = /#([a-zA-Z0-9\-]+)#/g;
+    function replaceParams(filters,node,context){
+      if ( !node || !node.nodeValue || !node.nodeValue.replace ||
+           !context || !context.data){
+        return;
+      }
+
+      node.nodeValue = node.nodeValue.replace(
+        PARAM_REGEXP,
+        function(match,param){
+          return context.data[param];
+        }
+      );
+    }
+
+    var node = element('span',{},'Welcome #name#');
+bezen.log.info( node.innerHTML, true);
+
+    ut(
+      [ topDownParsing, replaceParams],
+      node,
+      {data:
+        {name:'John Doe'}
+      }
+    );
+bezen.log.info( node.innerHTML, true);
+
+    var TEXT_NODE = 3;
+    assert.arrayEquals(
+      [
+        node.nodeType,
+        node.nodeName,
+        node.childNodes.length,
+        node.childNodes[0].nodeType,
+        node.childNodes[0].nodeValue
+      ],
+      [
+        ELEMENT_NODE,
+        'SPAN',
+        1,
+        TEXT_NODE,
+        'Welcome John Doe'
+      ],      "input node expected to be updated with param value replaced");
+
+    function replaceParamsInString(filters, string, data){
+      return string.replace(PARAM_REGEXP, function(match,param){
+        return data[param];
+      });
+    }
+
+    var greeting = ut(
+      [replaceParamsInString],
+      'Welcome #name#',
+      {name: 'John Doe'}
+    );
+    assert.equals( greeting, 'Welcome John Doe',
+                         "value expected to be replaced in string greeting");
+  }
+
+  var tests = {
+    testNamespace: testNamespace,
+    testApplyFilters: testApplyFilters
+  };
+
+  testrunner.define(tests, "lb.base.template");
+  return tests;
+
+}());
