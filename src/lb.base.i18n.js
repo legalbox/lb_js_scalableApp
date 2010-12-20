e2bc0d86798d7d173e44ad275d6d82b805592b2e
Eric Bréchemier <legalbox@eric.brechemier.name>
2010-12-20 12:10:00 +0100

    Fixed grammar in documentation of getProperty in lb.base.i18n
    I replaced 'to lookup' with 'to look up'.

--- src/lb.base.i18n.js
+++ src/lb.base.i18n.js
@@ -179,7 +179,7 @@ lb.base.i18n = lb.base.i18n || (function() {
     // The last two forms are equivalent, both matching a property
     // 'propertyName' nested in a property 'subsection' within a property
     // 'section' at top level of language properties. The array notation allows
-    // to lookup a property which would contain a dot in its name, without the
+    // to look up a property which would contain a dot in its name, without the
     // substitution to a section and subsection: ['no.substitution.done'].
     //
     // Parameters:
