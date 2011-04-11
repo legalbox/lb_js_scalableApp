8b8cf274ab4c01ef1fb699442255d6d7598e908d
Eric Bréchemier <legalbox@eric.brechemier.name>
2011-04-11 16:44:34 +0200

    Added unit tests and implementation of lb.base.object.has()
    The method is intended to replace unsafe tests for the presence of properties
    using type coercion:
    
        if (object && object.property) {
          // ...
        }
    
    can now be written:
    
        if ( has(object,'property') ){
          // ...
        }

--- src/lb.base.object.js
+++ src/lb.base.object.js
@@ -13,7 +13,7 @@
  * http://creativecommons.org/licenses/BSD/
  *
  * Version:
- * 2011-01-05
+ * 2011-04-11
  */
 /*requires lb.base.js */
 /*jslint white:false, plusplus:false */
@@ -23,11 +23,71 @@ lb.base.object = lb.base.object || (function() {
   // Builder of
   // Closure for lb.base.object module
 
-  // Declare alias
+  // Declare aliases
+      /*requires lb.base.type.js */
+  var is = lb.base.type.is,
       /*requires closure/goog.js */
-  var deepCopy = goog.cloneObject,
+      deepCopy = goog.cloneObject,
       /*requires closure/goog.object.js */
       shallowCopy = goog.object.clone;
+
+  function has(object, property){
+    // Function: has(object,property[,...]): boolean
+    // Check whether an object property is present and not null nor undefined.
+    //
+    // A chain of nested properties may be checked by providing more than two
+    // arguments. Only own properties are considered, properties inherited from
+    // the constructor's prototype are ignored.
+    //
+    // The intent of this method is to replace unsafe tests relying on type
+    // coercion for optional arguments or object properties:
+    // | function on(event){
+    // |   if (!event || !event.data || !event.data.value){
+    // |     // unsafe due to type coercion: all falsy values '', false, 0
+    // |     // are discarded, not just null and undefined
+    // |     return;
+    // |   }
+    // |   // ...
+    // | }
+    // with a safer test without type coercion:
+    // | function on(event){
+    // |   if (!has(event,'data','value'){
+    // |     // safe check: only null/undefined values are rejected;
+    // |     // besides, inherited properties are ignored.
+    // |     return;
+    // |   }
+    // |   // ...
+    // | }
+    //
+    // Parameters:
+    //   object - any, an object or any other value
+    //   property - string, the name of the property to look up
+    //   ...      - string, additional property names to check in turn
+    //
+    // Returns:
+    //   * false if no argument is provided or if the object is null or
+    //     undefined, whatever the number of arguments
+    //   * true if the object owns the full chain of nested properties for all
+    //     arguments provided, and the corresponding value is neither null nor
+    //     undefined
+    //   * false otherwise
+    if ( !is(object) ){
+      return false;
+    }
+    var i,
+        length;
+    for (i=1, length=arguments.length; i<length; i++){
+      property = arguments[i];
+      if ( !object.hasOwnProperty(property) ){
+        return false;
+      }
+      object = object[property];
+      if ( !is(object) ){
+        return false;
+      }
+    }
+    return true;
+  }
 
   function clone(object, deep){
     // Function: clone(object[,deep]): object
@@ -55,6 +115,7 @@ lb.base.object = lb.base.object || (function() {
   }
 
   return { // public API
+    has: has,
     clone: clone
   };
 }());
--- test/test.lb.base.object.js
+++ test/test.lb.base.object.js
@@ -4,7 +4,7 @@
  * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
  * Copyright: Legal-Box (c) 2010-2011, All Rights Reserved
  * License:   BSD License - http://creativecommons.org/licenses/BSD/
- * Version:   2011-01-05
+ * Version:   2011-04-11
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -29,6 +29,124 @@
 
     assert.isTrue( object.exists(window,'lb','base','object'),
                                     "lb.base.object namespace was not found");
+  }
+
+  function testHas(){
+    var ut = lb.base.object.has;
+
+    assert.isFalse( ut(),               "false expected without arguments");
+
+    // 1 argument
+    assert.isFalse( ut(null),           "false expected for null value");
+    assert.isFalse( ut(undefined),      "false expected for undefined value");
+
+    assert.isTrue( ut(false),           "true expected for false value");
+    assert.isTrue( ut(true),            "true expected for true value");
+
+    assert.isTrue( ut(-1),              "true expected for value -1");
+    assert.isTrue( ut(0),               "true expected for value 0");
+    assert.isTrue( ut(3),               "true expected for value 3");
+    assert.isTrue( ut(Math.PI),         "true expected for value PI");
+
+    assert.isTrue( ut(function(){}),    "true expected for a function");
+    assert.isTrue( ut(/abc/m),          "true expected for a regexp");
+
+    assert.isTrue( ut({}),              "true expected for empty object");
+    assert.isTrue( ut({id:42}),         "true expected for non-empty object");
+
+    assert.isTrue( ut([]),              "true expected for an empty array");
+    assert.isTrue( ut([1,2,3]),         "true expected for a non-empty array");
+
+    // 2 arguments
+    assert.isFalse( ut(null,'name'), "false expected for null value (2 args)");
+    assert.isFalse( ut(undefined,'name'),
+                                "false expected for undefined value (2 args)");
+
+    assert.isFalse( ut({},'missing'),
+                        "false expected for property missing in empty object");
+    assert.isFalse( ut({id:42},'missing'),
+                    "false expected for property missing in non-empty object");
+
+    assert.isFalse( ut({a:null},'a'),
+                            "false expected for null value found in property");
+    assert.isFalse( ut({a:undefined},'a'),
+                       "false expected for undefined value found in property");
+
+    assert.isTrue( ut({a:false},'a'),
+                            "true expected for false value found in property");
+    assert.isTrue( ut({a:true},'a'),
+                             "true expected for true value found in property");
+
+    assert.isTrue( ut({a:-1},'a'),
+                               "true expected for value -1 found in property");
+    assert.isTrue( ut({a:0},'a'),
+                                "true expected for value 0 found in property");
+    assert.isTrue( ut({a:3},'a'),
+                                "true expected for value 3 found in property");
+    assert.isTrue( ut({a:Math.PI},'a'),
+                               "true expected for value PI found in property");
+
+    assert.isTrue( ut({a:(function(){})},'a'),
+                             "true expected for a function found in property");
+    assert.isTrue( ut({a:/abc/m},'a'),
+                               "true expected for a regexp found in property");
+
+    assert.isTrue( ut({a:{}},'a'),
+                           "true expected for empty object found in property");
+    assert.isTrue( ut({a:{id:42}},'a'),
+                       "true expected for non-empty object found in property");
+
+    assert.isTrue( ut({a:[]},'a'),
+                         "true expected for an empty array found in property");
+    assert.isTrue( ut({a:[1,2,3]},'a'),
+                      "true expected for a non-empty array found in property");
+
+    // inherited properties
+    function F(){}
+    F.prototype = {
+      boolean: true,
+      string: 'abc',
+      array: ['a','b','c'],
+      constructor: F
+    };
+    assert.isTrue( ut(F.prototype,'boolean') &&
+                   ut(F.prototype,'string') &&
+                   ut(F.prototype,'array') &&
+                   ut(F.prototype,'constructor'),
+                  "assert: properties expected to be defined on F.prototype");
+
+    var object = new F();
+    assert.isFalse( ut(object,'boolean') ||
+                    ut(object,'string') ||
+                    ut(object,'array') ||
+                    ut(object,'constructor'),
+                                    "false expected for inherited properties");
+
+    // 3 arguments and more
+    var level1 = {
+      a: 'a',
+      b: 'b',
+      level2: {
+        a: 'a',
+        b: 'b',
+        level3: {
+          a: 'a',
+          level4: {
+            level5: "That's All Folks!"
+          }
+        }
+      },
+      c: 'c'
+    }
+
+    assert.isTrue( ut(level1,'level2'),
+                               "true expected for nested property 'level2'");
+    assert.isTrue( ut(level1,'level2','level3'),
+                               "true expected for nested property 'level3'");
+    assert.isTrue( ut(level1,'level2','level3','level4'),
+                               "true expected for nested property 'level4'");
+    assert.isTrue( ut(level1,'level2','level3','level4','level5'),
+                               "true expected for nested property 'level5'");
   }
 
   function testClone(){
@@ -63,6 +181,7 @@
 
   var tests = {
     testNamespace: testNamespace,
+    testHas: testHas,
     testClone: testClone
   };
 
