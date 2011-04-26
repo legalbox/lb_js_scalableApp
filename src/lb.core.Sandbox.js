ef7ec1c83962464ce0080d4e9eaf64bef00a01d1
Eric Bréchemier <legalbox@eric.brechemier.name>
2011-04-26 11:08:30 +0200

    Moved url methods from Sandbox to url plugin

--- src/lb.core.Sandbox.js
+++ src/lb.core.Sandbox.js
@@ -109,20 +109,13 @@ lb.core.Sandbox = function (id){
       trim = lb.base.string.trim,
       /*requires lb.base.log.js */
       log = lb.base.log.print,
-      /*requires lb.base.history.js */
-      history = lb.base.history,
-      setHash = history.setHash,
 
   // Private fields
 
-    // DOM element, the root of the box, carrying the module identifier.
-    // Used only in getBox(), to avoid multiple lookups of the same element.
-    // Initialized on first call to getBox().
-    box = null,
-
-    // function, the current listener set to onHashChange(), which will get
-    // replaced in a new call to onHashChange().
-    hashChangeCallback = null;
+      // DOM element, the root of the box, carrying the module identifier.
+      // Used only in getBox(), to avoid multiple lookups of the same element.
+      // Initialized on first call to getBox().
+      box = null;
 
   function getId(localId){
     // Function: sandbox.getId([localId]): string
@@ -213,69 +206,6 @@ lb.core.Sandbox = function (id){
       ancestor = ancestor.parentNode;
     }
     return false;
-  }
-
-  function getLocation(){
-    // Function: sandbox.url.getLocation(): object
-    // Get the properties of the current URL location
-    //
-    // Returns:
-    //   an object with a copy of properties commonly found on window.location
-    //   and document.location:
-    //     * href - string, the absolute URL of the current document
-    //     * protocol - string, the protocol part of the URL, e.g. 'http://'
-    //     * host - string, the host and port part of the url, e.g.
-    //              'example.com:8080' or often just 'example.com'
-    //     * hostname - the host name part of the URL, e.g. 'example:com'
-    //     * port - string, the port part of the URL, e.g. '8080' or often ''
-    //     * pathname - string, the relative path, e.g. '/2010/10/31/index.php'
-    //     * search - string, the query part of the url, e.g. '?param=value'
-    //     * hash - string, the local part of the url, e.g. '#anchor'.
-    //   These properties are read-only here and not shared with other modules.
-    var location = window.location;
-    return {
-      href: location.href,
-      protocol: location.protocol,
-      host: location.host,
-      hostname: location.hostname,
-      port: location.port,
-      pathname: location.pathname,
-      search: location.search,
-      hash: location.hash
-    };
-  }
-
-  // Function: sandbox.url.setHash(hash)
-  // Jump to a new local location by replacing the hash part of the URL.
-  //
-  // This method is used for local navigation, and ensures, in collaboration
-  // with the cross-browser history adapter module, that the back button
-  // of the browser works as expected.
-  //
-  // Parameter:
-  //   hash - string, the new local location, e.g. '#local/path'
-
-  // Note: setHash is an alias for lb.base.history.setHash
-
-  function onHashChange(callback){
-    // Function: sandbox.url.onHashChange(callback)
-    // Set a listener to observe changes in local part of the URL.
-    // Calling this method with a new callback will replace the listener
-    // previously set. Calling onHashChange(null) will remove the current
-    // listener altogether.
-    //
-    // Parameter:
-    //   callback - function, the callback(hash) function will be called once
-    //              for each subsequent change of hash. The hash parameter is a
-    //              string, decoded, starting with the '#' character.
-
-    if ( has(hashChangeCallback) ){
-      history.removeListener(hashChangeCallback);
-    }
-    hashChangeCallback = callback;
-    if ( has(callback) ){
-      history.addListener(callback);
-    }
   }
 
   // Function: sandbox.utils.has(object,property[,...]): boolean
@@ -483,11 +413,6 @@ lb.core.Sandbox = function (id){
   this.getId = getId;
   this.getBox = getBox;
   this.isInBox = isInBox;
-  this.url = {
-    getLocation: getLocation,
-    setHash: setHash,
-    onHashChange: onHashChange
-  };
   this.utils = {
     has: has,
     is: is,
