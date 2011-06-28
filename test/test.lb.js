0977537daf9eecfe391e2e22e65c0d7d6b4fdada
Eric Bréchemier <legalbox@eric.brechemier.name>
2011-06-28 11:50:51 +0200

    Updated unit tests for lb namespace, now using requireJS for module loading

--- test/test.lb.js
+++ test/test.lb.js
@@ -6,28 +6,38 @@
  *   o Marc Delhommeau <marc.delhommeau@legalbox.com>
  * Copyright: Legal-Box (c) 2010-2011, All Rights Reserved
  * License:   BSD License - http://creativecommons.org/licenses/BSD/
- * Version:   2011-06-14
+ * Version:   2011-06-28
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
  */
 
 /*jslint white:false, onevar:false, plusplus:false */
-/*global window */
+/*global window, define, require */
 define(
   [
     "bezen.org/bezen.assert",
     "bezen.org/bezen.object",
-    "bezen.org/bezen.testrunner"
+    "bezen.org/bezen.testrunner",
+    "lb/lb"
   ],
-  function(assert,object,testrunner) {
+  function(
+    assert,
+    object,
+    testrunner,
+    lb
+  ) {
   // Builder of
-  // Closure object for Test of Legal-Box Web Application
+  // Closure object for Test of namespace lb
 
   function testNamespace(){
 
-    assert.isTrue( object.exists(window,'lb'),
-                                                "lb namespace was not found");
+    assert.isTrue( object.exists(lb),      "lb was not found in dependencies");
+
+    if ( object.exists(window) ) {
+      assert.isTrue( object.exists(window,'lb'),
+                                       "lb namespace was not found in window");
+    }
   }
 
   var tests = {
