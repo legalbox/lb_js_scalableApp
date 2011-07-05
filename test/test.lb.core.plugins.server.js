b03a3a2d0e63ebdc346c93bad7e4b49dce76315f
Eric Bréchemier <legalbox@eric.brechemier.name>
2011-07-05 10:12:29 +0200

    Fixed typo: MockXmlHttp instead of MockXmlHtpp

--- test/test.lb.core.plugins.server.js
+++ test/test.lb.core.plugins.server.js
@@ -4,7 +4,7 @@
  * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
  * Copyright: Legal-Box (c) 2010-2011, All Rights Reserved
  * License:   BSD License - http://creativecommons.org/licenses/BSD/
- * Version:   2011-06-30
+ * Version:   2011-07-05
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -27,7 +27,7 @@ define(
     array,
     object,
     testrunner,
-    MockXmlHtpp,
+    MockXmlHttp,
     Sandbox,
     pluginsServer
   ){
