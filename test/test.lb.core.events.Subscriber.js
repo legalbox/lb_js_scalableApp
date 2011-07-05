f3b3a30b6b0fac8261a2db091a5bca0d77bab469
Eric Bréchemier <legalbox@eric.brechemier.name>
2011-07-05 14:39:10 +0200

    Fixed dependency: lb.core.events.Subscriber, not lb.core.Subscriber

--- test/test.lb.core.events.Subscriber.js
+++ test/test.lb.core.events.Subscriber.js
@@ -4,7 +4,7 @@
  * Author:    Eric Bréchemier <legalbox@eric.brechemier.name>
  * Copyright: Legal-Box (c) 2010-2011, All Rights Reserved
  * License:   BSD License - http://creativecommons.org/licenses/BSD/
- * Version:   2011-06-30
+ * Version:   2011-07-05
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -18,7 +18,7 @@ define(
     "bezen.org/bezen.assert",
     "bezen.org/bezen.object",
     "bezen.org/bezen.testrunner",
-    "lb/lb.core.Subscriber"
+    "lb/lb.core.events.Subscriber"
   ],
   function(
     bezen,
