77c1f819fddae4c40bc90db846cfdd3442e61c3a
Marc Delhommeau <marc.delhommeau@legalbox.com>
2011-07-12 10:46:52 +0200

    Updated jslint directive, removing common options, using new option vars
    The onvar:false option is now vars:true, to allow multiple var declarations
    in the same function.

--- test/test.lb.base.history.js
+++ test/test.lb.base.history.js
@@ -7,13 +7,13 @@
  *
  * Copyright: Legal-Box (c) 2010-2011, All Rights Reserved
  * License:   BSD License - http://creativecommons.org/licenses/BSD/
- * Version:   2011-07-07
+ * Version:   2011-07-12
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
  */
 
-/*jslint white:false, onevar:false, plusplus:false */
+/*jslint vars:true */
 /*global define, window, lb, setTimeout, document */
 define(
   [
